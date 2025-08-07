29031a9486b7f1d138908599efea1e22735fc209
Camila0517 <camiest0501@gmail.com>
2025-08-07 12:51:25 -0600

    Soy Andrea y he creado el menu en la main class, Principal

--- src/org/utl/ldsm304/neatgi/vista/Principal.java
+++ src/org/utl/ldsm304/neatgi/vista/Principal.java
@@ -4,6 +4,9 @@
  */
 package org.utl.ldsm304.neatgi.vista;
 
+import java.util.Scanner;
+import org.utl.ldsm304.neatgi.controlador.Ejercicio2;
+
 /**
  *
  * @author camie
@@ -14,7 +17,27 @@ public class Principal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        MostrarMenu();
+    }
+    
+    public static void MostrarMenu(){
+        Scanner sc= new Scanner(System.in);
+        Ejercicio2 e2 = new Ejercicio2();
+        System.out.println("------MENU------");
+        System.out.println("1-Análisis de Precios en Matriz");
+        System.out.println("2-Análisis de Cadenas en Matriz");
+        
+        int option=sc.nextInt();
+        switch(option){
+            case 1:
+                e2.equals(e2);
+                break;
+            case 2:
+                System.out.println(e2.getClass());
+                break;
+            default:
+                System.out.println("Error");
+        }
     }
     
 }
