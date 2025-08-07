7a719252e21668a243ec150782a4b3029252c55e
Ximena-2007 <ximenasfausto@gmail.com>
2025-08-07 14:35:26 -0600

    Soy Ximena y actualice el codigo

--- src/org/utl/ldsm304/neatgi/controlador/Ejercicio2.java
+++ src/org/utl/ldsm304/neatgi/controlador/Ejercicio2.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
  * @author camie
  */
 public class Ejercicio2 {
-      private String[][] Frutas = {
+      public String[][] Frutas = {
         {"Mango", "Sandía", "Pera"},
         {"Fresa", "Uva", "Kiwi"},
         {"Aguacate", "Plátano", "Oliva"}
