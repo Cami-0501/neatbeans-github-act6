5cb70054db2d4ad164c399bc685b566c71a65de4
Camila0517 <camiest0501@gmail.com>
2025-08-07 14:14:29 -0600

    Soy Andrea y he actualizado del menu

--- src/org/utl/ldsm304/neatgi/vista/Principal.java
+++ src/org/utl/ldsm304/neatgi/vista/Principal.java
@@ -5,6 +5,7 @@
 package org.utl.ldsm304.neatgi.vista;
 
 import java.util.Scanner;
+import org.utl.ldsm304.neatgi.controlador.Ejercicio1;
 import org.utl.ldsm304.neatgi.controlador.Ejercicio2;
 
 /**
@@ -23,6 +24,7 @@ public class Principal {
     public static void MostrarMenu(){
         Scanner sc= new Scanner(System.in);
         Ejercicio2 e2 = new Ejercicio2();
+        Ejercicio1 e1 = new Ejercicio1();
         System.out.println("------MENU------");
         System.out.println("1-Análisis de Precios en Matriz");
         System.out.println("2-Análisis de Cadenas en Matriz");
@@ -30,10 +32,46 @@ public class Principal {
         int option=sc.nextInt();
         switch(option){
             case 1:
-                e2.equals(e2);
-                break;
+                System.out.println("------MENU------");
+                System.out.println("1-Obtener el número que más se repite.");
+                System.out.println("2-Obtener la cantidad de números pares."); 
+                System.out.println("3-Obtener la cantidad de números impares");
+                System.out.println("4-Obtener la suma de los números que están en la diagonal."); 
+                System.out.println("5-Obtener la cantidad de números múltiplos de 5.");
+                option=sc.nextInt();
+                switch(option){
+                    case 1:
+                        
+                    case 2:
+                        
+                    case 3:
+                    
+                    case 4:
+                        
+                    case 5:    
+                        
+                    break;
+            default:
+                System.out.println("Error");
+                }
             case 2:
-                System.out.println(e2.getClass());
+                System.out.println("------MENU------");
+                System.out.println("1-Devuelva las cadenas que comienzan con la letra “o”");
+                System.out.println("2-Devuelva la cadena que tenga las 5 vocales. "); 
+                System.out.println("3-Devuelva las cadenas que tienen la silaba “sa”");
+                option=sc.nextInt();
+                switch(option){
+                    case 1:
+                        
+                    case 2:
+                        
+                    case 3:
+                        
+                        
+                    break;
+            default:
+                System.out.println("Error");
+                }
                 break;
             default:
                 System.out.println("Error");
